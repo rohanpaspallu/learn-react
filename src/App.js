037e0e7d79497917000fcb08e1c3464b3161cd00
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2020-05-12 18:05:36 -0400

    Working with states, deleting elements from the list of states

--- src/App.js
+++ src/App.js
@@ -47,6 +47,13 @@ class App extends Component{
 
   }
 
+  deletePersonHandler=(personIndex)=>{
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex,1);
+    this.setState({persons: persons});
+  }
+
 
   render(){
 
@@ -54,8 +61,8 @@ class App extends Component{
     if(this.state.showPersons){
       persons = (
         <div>
-          {this.state.persons.map(person =>{
-            return <Person name = {person.name} age = {person.age}></Person>
+          {this.state.persons.map((person,index) =>{
+            return <Person click={()=>this.deletePersonHandler(index)} name = {person.name} age = {person.age}></Person>
           })}
         </div>
       );
