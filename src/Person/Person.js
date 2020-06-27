46e38c86b7aa6e8eb305c65c8d65532f6dbc004c
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2020-06-27 13:29:22 -0400

    Latest changes

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,24 +2,6 @@ import React from 'react';
 
 import classes from './Person.css';
 
-<<<<<<< HEAD
-const person = ( props ) => {
-    const rnd = Math.random();
-
-    // if ( rnd > 0.7 ) {
-    //     throw new Error( 'Something went wrong' );
-    // }
-    return (
-        <div className={classes.Person}>
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
-        </div>
-    )
-};
-
-export default person;
-=======
 const person = props => {
   return (
     <div className={classes.Person}>
@@ -33,4 +15,3 @@ const person = props => {
 };
 
 export default person;
->>>>>>> af44a7ae24fcbf6f71294dd53d8a00e1fe797689
