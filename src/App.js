c3814292e419fc6f7acd6f4bc1bbd7aa2ae3a1e5
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2020-06-27 13:24:05 -0400

    Latest changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
-
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
-
-const StyledButton = styled.button`
-  background-color: ${props => props.alt ? 'red' : 'green'};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
-  
-  &:hover {
-    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-    color: black;
-  }
-`;
 
 class App extends Component {
   state = {
@@ -28,12 +12,12 @@ class App extends Component {
     ],
     otherState: 'some other value',
     showPersons: false
-  };
+  }
 
-  nameChangedHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(p => {
+  nameChangedHandler = ( event, id ) => {
+    const personIndex = this.state.persons.findIndex( p => {
       return p.id === id;
-    });
+    } );
 
     const person = {
       ...this.state.persons[personIndex]
@@ -46,77 +30,58 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
-  };
+    this.setState( { persons: persons } );
+  }
 
-  deletePersonHandler = personIndex => {
+  deletePersonHandler = ( personIndex ) => {
     // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
-    persons.splice(personIndex, 1);
-    this.setState({ persons: persons });
-  };
+    persons.splice( personIndex, 1 );
+    this.setState( { persons: persons } );
+  }
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({ showPersons: !doesShow });
-  };
+    this.setState( { showPersons: !doesShow } );
+  }
 
-  render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    };
-
+  render () {
     let persons = null;
+    let btnClass = '';
 
-    if (this.state.showPersons) {
+    if ( this.state.showPersons ) {
       persons = (
         <div>
-          {this.state.persons.map((person, index) => {
-            return (
-              <ErrorBoundary key={person.id}>
-                <Person
-                  click={() => this.deletePersonHandler(index)}
-                  name={person.name}
-                  age={person.age}
-                  
-                  changed={event => this.nameChangedHandler(event, person.id)}
-              /></ErrorBoundary>
-            );
-          })}
+          {this.state.persons.map( ( person, index ) => {
+            return <ErrorBoundary key={person.id}>
+              <Person
+                click={() => this.deletePersonHandler( index )}
+                name={person.name}
+                age={person.age}
+                changed={( event ) => this.nameChangedHandler( event, person.id )} />
+            </ ErrorBoundary>
+          } )}
         </div>
       );
 
-      // style.backgroundColor = 'red';
-      // style[':hover'] = {
-      //   backgroundColor: 'salmon',
-      //   color: 'black'
-      // };
+      btnClass = classes.Red;
     }
 
-    const classes = [];
-    if (this.state.persons.length <= 2) {
-      classes.push('red'); // classes = ['red']
+    const assignedClasses = [];
+    if ( this.state.persons.length <= 2 ) {
+      assignedClasses.push( classes.red ); // classes = ['red']
     }
-    if (this.state.persons.length <= 1) {
-      classes.push('bold'); // classes = ['red', 'bold']
+    if ( this.state.persons.length <= 1 ) {
+      assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>
-          Toggle Persons
-        </StyledButton>
+        <p className={assignedClasses.join( ' ' )}>This is really working!</p>
+        <button
+          className={btnClass}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,42 +1,20 @@
 import React from 'react';
-import './Person.css';
-import styled from 'styled-components';
-// import Radium from 'radium';
 
-const StyledDiv = styled.div`
-    width:  60%;
-    margin : 30px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    background-color: rgb(72, 255, 0);
-    padding: 16px;
-    text-align: center;
-        
-    @media (min-width: 500px){
-        width: 450px;
-    }
-`;
+import classes from './Person.css';
 
-
-const person = (props) => {
-
+const person = ( props ) => {
     const rnd = Math.random();
 
-    if(rnd> 0.7){
-        throw new Error('something went wrong !!!');
-    }
-
+    // if ( rnd > 0.7 ) {
+    //     throw new Error( 'Something went wrong' );
+    // }
     return (
-     
-        // <div className="Person" style={style}>
-
-        <StyledDiv>
-            <p onClick={props.click}>I'm a person, my name is {props.name} and my age is {props.age} </p>
+        <div className={classes.Person}>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name}></input>
-        </StyledDiv>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </div>
     )
 };
 
-// export default Radium(person);
 export default person;
